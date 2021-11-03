4259c19dfc8420d2b0dce5f11856d2f8790c331f
kaqbe <84801823+kaqbe@users.noreply.github.com>
2021-11-03 17:18:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 var menu = document.getElementById('menu');
-
+const mediaQueryPhone = window.matchMedia('(max-width: 600px)');
+var h_in = document.getElementById('h_in');
 
 function hide_menu() {
-    menu.style.right = '-51%';
-    menu.style.visibility = 'hidden';
+
+    if (mediaQueryPhone.matches) {
+
+        menu.style.height = '0%';
+
+    } else {
+
+        menu.style.right = '-51%';
+        menu.style.visibility = 'hidden';
+
+    }
 }
 
 function show_menu() {
-    menu.style.right = '0';
-    menu.style.visibility = 'visible';
+
+    if (mediaQueryPhone.matches) {
+
+        if (menu.style.height == '80%') {
+            hide_menu()
+        } else {
+            menu.style.height = '80%';
+            menu.style.visibility = 'visible';
+        }
+
+    } else {
+
+        menu.style.right = '0';
+        menu.style.visibility = 'visible';
+        menu.style.height = '100%';
+
+    }
 }
